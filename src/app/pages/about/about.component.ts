a5cd2a3ebb2d9b1bdfeb47183c8372d865fe1620
251536 Jdrzejczak Mateusz <251536@edu.p.lodz.pl>
2026-06-29 22:44:18 +0200

    feat: implement About page

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,3 +1,15 @@
-import { Component } from '@angular/core';
-@Component({ selector: 'app-about', standalone: true, template: '<div></div>' })
-export class AboutComponent {}
+import { Component, inject } from '@angular/core';
+import { TranslationService } from '../../core/services/translation.service';
+import { SectionHeaderComponent } from '../../shared/components/section-header/section-header.component';
+
+@Component({
+  selector: 'app-about',
+  standalone: true,
+  imports: [SectionHeaderComponent],
+  templateUrl: './about.component.html',
+  styleUrl: './about.component.scss',
+})
+export class AboutComponent {
+  private ts = inject(TranslationService);
+  protected tr = this.ts.tr;
+}
