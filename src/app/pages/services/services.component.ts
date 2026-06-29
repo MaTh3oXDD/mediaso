e8e2204be161c1fe7cca9bc7bf5290f989fb9e5b
251536 Jdrzejczak Mateusz <251536@edu.p.lodz.pl>
2026-06-29 22:41:44 +0200

    feat: implement Services page

--- src/app/pages/services/services.component.ts
+++ src/app/pages/services/services.component.ts
@@ -1,3 +1,16 @@
-import { Component } from '@angular/core';
-@Component({ selector: 'app-services', standalone: true, template: '<div></div>' })
-export class ServicesComponent {}
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { TranslationService } from '../../core/services/translation.service';
+import { SectionHeaderComponent } from '../../shared/components/section-header/section-header.component';
+
+@Component({
+  selector: 'app-services',
+  standalone: true,
+  imports: [RouterLink, SectionHeaderComponent],
+  templateUrl: './services.component.html',
+  styleUrl: './services.component.scss',
+})
+export class ServicesComponent {
+  private ts = inject(TranslationService);
+  protected tr = this.ts.tr;
+}
